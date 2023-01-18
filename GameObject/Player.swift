506cd34aae9c73b6a990de59e4a69eedff41f0d5
charrrleee <charlcpy@gmail.com>
2023-01-18 17:12:15 -0500

    init

---
+++ GameObject/Player.swift
@@ -0,0 +1,67 @@
+//
+//  Player.swift
+//  MAPD722 - ICE1
+//
+//  Created by Charlene Cheung on 16/1/2023.
+//
+
+import Foundation
+
+
+
+import GameplayKit
+import SpriteKit
+
+class Player : GameObject
+{
+    // initializer
+    init()
+    {
+        super.init(imageString: "plane", initialScale: 2.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // LifeCycle Functions
+    override func Start()
+    {
+        zPosition = 2
+        Reset()
+    }
+    
+    override func Update()
+    {
+        CheckBounds()
+    }
+    
+    override func CheckBounds()
+    {
+        // constrain the player on the left boundary
+        if(position.x <= -255)
+        {
+            position.x = -255
+        }
+        
+        // constrain the player on the right boundary
+        if(position.x >= 255)
+        {
+            position.x = 255
+        }
+    }
+    
+    override func Reset()
+    {
+        position.y = -495
+    }
+    
+    
+    func TouchMove(newPos: CGPoint)
+    {
+        position = newPos
+    }
+    
+    
+}
